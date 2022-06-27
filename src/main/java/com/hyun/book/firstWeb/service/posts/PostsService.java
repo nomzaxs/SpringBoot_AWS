813a83d9f095a761898c5c94273651ed9790c29c
nomzaxs <nomzaxs@gmail.com>
2022-06-27 17:46:14 +0900

    3강 코드 완

---
+++ src/main/java/com/hyun/book/firstWeb/service/posts/PostsService.java
@@ -0,0 +1,36 @@
+package com.hyun.book.firstWeb.service.posts;
+
+import com.hyun.book.firstWeb.domain.posts.Posts;
+import com.hyun.book.firstWeb.domain.posts.PostsRepository;
+import com.hyun.book.firstWeb.web.dto.PostsResponseDto;
+import com.hyun.book.firstWeb.web.dto.PostsSaveRequestDto;
+import com.hyun.book.firstWeb.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService{
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
